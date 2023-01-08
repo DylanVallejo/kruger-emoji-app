208941ef081b31a27771854d5ec439473fc7ff9f
DylanVallejo <dylan1811vallejo@gmail.com>
2023-01-08 02:08:53 -0500

    Initaial components and structure folder,  an input being focused by useRef

---
+++ src/components/emojiPicker/EmojiPicker.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import EmojiPickerInput from './EmojiPickerInput';
+
+
+const  EmojiPicker = () => {
+    return (
+        <div>
+            <div>EmojiPicker Component</div>
+            {/* <EmojiPickerInput/> */}
+        </div>
+    )
+}
+
+export default EmojiPicker;
